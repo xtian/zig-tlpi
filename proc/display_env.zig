0d69d502ae6c6fcc1f421160fe69cce4a1d54910
Christian Wesselhoeft <christian.wesselhoeft@cpowerenergymanagement.com>
2022-10-06 15:21:24 -0600

    proc/display_env.zig: Update for 0.10

--- proc/display_env.zig
+++ proc/display_env.zig
@@ -1,10 +1,10 @@
 const std = @import("std");
 
 pub fn main() !u8 {
-    const stdout = std.io.getStdOut().outStream();
+    const stdout = std.io.getStdOut().writer();
 
     for (std.os.environ) |ep| {
-        try stdout.print("{}\n", .{ ep });
+        try stdout.print("{s}\n", .{ep});
     }
 
     return 0;
