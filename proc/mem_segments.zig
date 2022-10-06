c3498d7321e856ec31b361c3e94330f6a52fe05b
Christian Wesselhoeft <christian.wesselhoeft@cpowerenergymanagement.com>
2022-10-06 15:30:20 -0600

    proc/mem_segments.zig: Update for 0.10

--- proc/mem_segments.zig
+++ proc/mem_segments.zig
@@ -1,16 +1,18 @@
 const std = @import("std");
 const c = std.c;
-const warn = std.debug.warn;
+const print = std.debug.print;
 
-const glob_buf : [65536]u8 = undefined; // uninitialized data segment
-const primes = [_]usize{2, 3, 5, 7}; // initialized data segment
+const glob_buf: [65536]u8 = undefined; // uninitialized data segment
+const primes = [_]usize{ 2, 3, 5, 7 }; // initialized data segment
 
 pub fn main() !u8 { // argv allocated in frame for main()
     const key = 9973; // initialized data segment
-    const mbuf : [10240000]u8 = undefined; // uninitialized data segment
-    var p : *c_void = undefined; // allocated in frame for main()
+    const mbuf: [10240000]u8 = undefined; // uninitialized data segment
+    var p: *anyopaque = undefined; // allocated in frame for main()
 
-    p = c.malloc(1024); // points to memory in heap segment
+    _ = mbuf;
+
+    p = c.malloc(1024).?; // points to memory in heap segment
 
     do_calc(key);
 
@@ -25,10 +27,10 @@ fn square(x: usize) usize { // allocated in frame in square()
 }
 
 fn do_calc(val: usize) void { // allocated in frame for do_calc
-    warn("The square of {} is {}\n", .{val, square(val)});
+    print("The square of {} is {}\n", .{ val, square(val) });
 
     if (val < 1000) {
         const t = val * val * val; // allocated in frame for do_calc();
-        warn("The cube of {} is {}\n", .{val, t});
+        print("The cube of {} is {}\n", .{ val, t });
     }
 }
