72b1bd9bc6916f17f3f2d30903e8f28342dc1892
Christian Wesselhoeft <christian.wesselhoeft@cpowerenergymanagement.com>
2022-10-06 15:42:38 -0600

    fileio/copy.zig: Update for 0.10

--- fileio/copy.zig
+++ fileio/copy.zig
@@ -1,21 +1,21 @@
 const std = @import("std");
 const os = std.os;
-const warn = std.debug.warn;
+const print = std.debug.print;
 
 const BUFFER_SIZE = 1024;
 
-const OPEN_FLAGS = os.O_CREAT | os.O_WRONLY | os.O_TRUNC;
-const FILE_PERMS = os.S_IRUSR | os.S_IWUSR | os.S_IRGRP | os.S_IWGRP | os.S_IROTH | os.S_IWOTH; // rw-rw-rw-
+const OPEN_FLAGS = os.O.CREAT | os.O.WRONLY | os.O.TRUNC;
+const FILE_PERMS = os.S.IRUSR | os.S.IWUSR | os.S.IRGRP | os.S.IWGRP | os.S.IROTH | os.S.IWOTH; // rw-rw-rw-
 
 pub fn main() !u8 {
     if (os.argv.len != 3 or std.cstr.cmp(os.argv[1], "--help") == 0) {
-        warn("Usage: old-file new-file\n", .{});
+        print("Usage: old-file new-file\n", .{});
         return 1;
     }
 
     // Open input and output files
 
-    const input_fd = try os.openZ(os.argv[1], os.O_RDONLY, 0);
+    const input_fd = try os.openZ(os.argv[1], os.O.RDONLY, 0);
     defer os.close(input_fd);
 
     const output_fd = try os.openZ(os.argv[2], OPEN_FLAGS, FILE_PERMS);
@@ -30,15 +30,15 @@ pub fn main() !u8 {
 
         if (os.write(output_fd, buffer[0..bytes_read])) |bytes_written| {
             if (bytes_written != bytes_read) {
-                warn("Error: couldn't write whole buffer ({} != {})", .{ bytes_written, bytes_read });
+                print("Error: couldn't write whole buffer ({} != {})", .{ bytes_written, bytes_read });
                 return 1;
             }
         } else |err| {
-            warn("Error: write {}", .{err});
+            print("Error: write {}", .{err});
             return 1;
         }
     } else |err| {
-        warn("Error: read {}", .{err});
+        print("Error: read {}", .{err});
         return 1;
     }
 
